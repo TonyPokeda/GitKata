2923bd10c63b26a6d82521504a3275101a1558ff
TonyPokeda <vowasaburofff@mail.ru>
2025-04-07 20:53:34 +0400

    Add files via upload

---
+++ 3.1.2.SpringSecurity/src/main/java/web/service/RoleServiceImpl.java
@@ -0,0 +1,48 @@
+package web.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.entity.Role;
+import web.repositories.RoleRepository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+    @Transactional
+    @Override
+    public Optional<Role> findByName(String roleName) {
+        return roleRepository.findByName(roleName);
+    }
+
+    @Transactional
+    @Override
+    public Role save(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Transactional
+    @Override
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public List<Role> findRolesByIds(List<Long> roleIds) {
+        return roleRepository.findAllById(roleIds);
+    }
+
+
+}
---
+++ 3.1.2.SpringSecurity/src/main/java/web/service/UserService.java
@@ -0,0 +1,22 @@
+package web.service;
+
+
+
+import web.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User getUserById(Long id);
+
+    User findByUsername(String name);
+
+    List<User> getAllUsers();
+
+    void saveUser(User user);
+
+    void deleteUser(Long id);
+
+    void updateUser(Long id, User user,List<Long> roleIds);
+}
